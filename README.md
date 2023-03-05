3cf3eaea3c9ad720d1e041be0be8febcdebb1f9b
dh <1078680188@qq.com>
2023-03-05 22:42:14 +0800

    初版上传

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spock-reports-example
+
+根据`spock-reports`自定义模板
---
+++ src/test/groovy/example/Example.groovy
@@ -0,0 +1,34 @@
+package example
+
+import spock.lang.Specification
+import spock.lang.Title
+import spock.lang.Unroll
+@Title("sdas")
+class Example extends Specification {
+    def "This test is passing"() {
+        when: "Setting up the number"
+        def one = 1
+
+        then: "Doing the check"
+        1 == one
+    }
+
+    def "This test is failing"() {
+        when: "Setting up the number"
+        def one = 1
+
+        then: "Doing the check"
+        2 == one
+    }
+
+    @Unroll("This test is data driven. Using number #number")
+    def "This test is data driven"() {
+        when: "Setting up the number"
+        number += 1
+
+        then: "Doing the check"
+        2 == number
+        where:
+        number << [1, 2, 3]
+    }
+}
